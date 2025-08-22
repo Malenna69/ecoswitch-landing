9d29fb4e828b77b6296b35de067e6f37779d02b3
Chafique <zmalenna20@gmail.com>
2025-08-23 00:01:45 +0200

    Update next.config.ts

--- ecoswitch-landing/next.config.ts
+++ ecoswitch-landing/next.config.ts
@@ -1,7 +1,12 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      { source: "/", destination: "/landing-b", permanent: false },
+    ];
+  },
 };
 
 export default nextConfig;
